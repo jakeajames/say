edf7a74172e7df902d886026b2084924cd768df9
Jake James <jakeashacks@gmail.com>
2018-04-02 19:04:41 +0200

    Update main.m

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 int main(int argc, char **argv, char **envp) {
 	if (argc < 2) {
 		printf("Ayeee, we need something to say\n");
-		printf("	%s somethingToSay", argv[0]);
+		printf("	%s somethingToSay\n", argv[0]);
 		return -1;
 	}
 	CPDistributedMessagingCenter *messagingCenter;
